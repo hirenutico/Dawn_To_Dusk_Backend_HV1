16cb2adb7412f76fcc6f6373de401f150ec57697
Hiren Joshi <hiren@nvestbank.com>
2021-08-15 10:56:41 +0530

    Update some string
    Update the phone no
    Update number to string

--- src/Auth/AuthModel.ts
+++ src/Auth/AuthModel.ts
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     fullname: { type: String,  required: true },
     email: { type: String, unique: true, required: true },
-    mobile: { type: Number, unique: true, required: true },
+    mobile: { type: String, unique: true, required: true },
     countryCode: { type: String, required: true },
-    otp_token:{type:Number},
-    verify_otp:{type:Boolean,default:false}
+    otp_token: { type: Number },
+    verify_otp: { type: Boolean, default: false}
 });
 
 schema.set('toJSON', { virtuals: true });
